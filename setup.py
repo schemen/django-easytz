55472d3396458242f76a33a557300e7d598fc5ff
Andrew Buchan <andyhasit@gmail.com>
2017-12-22 12:14:56 +0000

    Bumped version & changed url in setup.py

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'django-easytz',
-    version = '0.4',
+    version = '0.5',
     description = 'Timezone localization without any thinking or doing whatsoever.',
     long_description = open('README.md').read(),
     author = 'James Friedman',
     author_email = 'jamesmfriedman@gmail.com',
-    url = 'https://github.com/jamesmfriedman/django-easytz/',
-    download_url = 'https://github.com/jamesmfriedman/django-easytz/',
+    url = 'https://github.com/andyhasit/django-easytz/',
+    download_url = 'https://github.com/andyhasit/django-easytz/',
     license = 'Apache',
     keywords = ('timezone','tzinfo', 'datetime', 'jamesmfriedman', 'time', 'django', 'django-timezones', 'django-tz', 'django-easytz'),
     packages = find_packages(),
     package_data = {'easytz': ['static/easytz/js/*']},
     tests_require = (
-        'django>=1.4',
+        'django>=2.0',
     ),
     include_package_data = True,
     zip_safe = False,  # because we're including media that Django needs
